e70969f039d4c53284abd73ed18423a665efcade
Kien Pham <125080980+kien-cs@users.noreply.github.com>
2023-05-10 11:00:08 -0700

    Add new example for Lambda layer + otel + CodeSee (#9)
    * init commit for a working otel + lambda layer
    
    * update
    
    * remove unneeded role for xray

---
+++ serverless-lambda-otel-example/README.md
@@ -0,0 +1,15 @@
+# serverless-lambda-otel-example
+
+Example Serverless + Lambda + Open Telemetry integration
+
+## Install packages
+
+```
+npm i
+```
+
+## Deploy Lambda
+
+```
+sls deploy
+```
---
+++ serverless-lambda-otel-example/index.js
@@ -0,0 +1,14 @@
+module.exports.handler = async (event) => {
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "hello from serverless-lambda-otel-example lambda",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
