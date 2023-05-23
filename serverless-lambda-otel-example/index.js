c4cfc8275187e914f4398a87166bc58a5ff01185
Kien Pham <kien@codesee.io>
2023-05-23 16:00:53 -0700

    add direct auto instrument example

--- serverless-lambda-otel-example/index.js
+++ serverless-lambda-otel-example/index.js
@@ -1,3 +1,36 @@
+// Require dependencies
+const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
+const opentelemetry = require("@opentelemetry/api");
+const { Resource } = require("@opentelemetry/resources");
+const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+const { ConsoleSpanExporter, BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+
+// This registers all instrumentation packages
+registerInstrumentations({
+  instrumentations: [
+    getNodeAutoInstrumentations()
+  ],
+});
+
+const resource =
+  Resource.default().merge(
+    new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: "serverless-lambda-otel-example",
+      [SemanticResourceAttributes.SERVICE_VERSION]: "0.1.0",
+    })
+  );
+
+const provider = new NodeTracerProvider({
+    resource: resource,
+});
+const exporter = new ConsoleSpanExporter();
+const processor = new BatchSpanProcessor(exporter);
+provider.addSpanProcessor(processor);
+
+provider.register();
+
 module.exports.handler = async (event) => {
 
   return {
