b93e234c09550cd41754ea45769822ab3f032929
Kien Pham <kien@codesee.io>
2023-06-01 10:28:37 -0700

    update

--- serverless-lambda-otel-example/collector.js
+++ serverless-lambda-otel-example/collector.js
@@ -1,32 +1,29 @@
 const grpc = require('@grpc/grpc-js');
-const opentelemetry = require('@opentelemetry/sdk-node');
+const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
-const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-proto");
+const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-grpc");
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
-const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-base");
-const process = require('process');
 
 // For troubleshooting, set the log level to DiagLogLevel.DEBUG
 const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
-const codeseeToken = process.env.CODESEE_TOKEN;
+const codeseeToken = process.env.CODESEE_TOKEN; // !! CodeSee auth token
 const metadata = new grpc.Metadata();
 metadata.set('Authorization', `Bearer ${codeseeToken}`);
 
-const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
-  // traceExporter: new OTLPTraceExporter({
-  //   url: "https://in-otel.codesee.io:443/v1/traces",
-  //   credentials: grpc.credentials.createSsl(),
-  //   metadata,
-  // }),
+const sdk = new NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    url: "https://in-otel.codesee.io:443/v1/traces", // CodeSee endpoint
+    credentials: grpc.credentials.createSsl(),
+    metadata,
+  }),
   resource: Resource.default().merge(
     new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: "local-test",  // !! NAME YOUR SERVICE !!
-      [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: "staging", // !! SET YOUR ENVIRONMENT
-      [SemanticResourceAttributes.SERVICE_VERSION]: "0.1.0", // (optional) set version
+      [SemanticResourceAttributes.SERVICE_NAME]: process.env.SERVICE_NAME,  // !! NAME YOUR SERVICE !!
+      [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: process.env.DEPLOYMENT_ENVIRONMENT, // !! SET YOUR ENVIRONMENT
+      [SemanticResourceAttributes.SERVICE_VERSION]: process.env.SERVICE_VERSION, // (optional) set version
     })
   ),
   instrumentations: [getNodeAutoInstrumentations()],
