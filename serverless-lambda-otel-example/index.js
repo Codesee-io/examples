c44a21cdde809ccb1506c8c9acac118349426ad5
Kien Pham <kien@codesee.io>
2023-05-31 17:25:37 -0700

    adding example to use otel js directly

--- serverless-lambda-otel-example/index.js
+++ serverless-lambda-otel-example/index.js
@@ -1,38 +1,19 @@
-// Require dependencies
-const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
-const opentelemetry = require("@opentelemetry/api");
-const { Resource } = require("@opentelemetry/resources");
-const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
-const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-const { ConsoleSpanExporter, BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+'use strict'
+require("./collector");
 
-// This registers all instrumentation packages
-registerInstrumentations({
-  instrumentations: [
-    getNodeAutoInstrumentations()
-  ],
-});
+const http = require('http');
 
-const resource =
-  Resource.default().merge(
-    new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: "serverless-lambda-otel-example",
-      [SemanticResourceAttributes.SERVICE_VERSION]: "0.1.0",
-    })
-  );
-
-const provider = new NodeTracerProvider({
-    resource: resource,
-});
-const exporter = new ConsoleSpanExporter();
-const processor = new BatchSpanProcessor(exporter);
-provider.addSpanProcessor(processor);
-
-provider.register();
+// make a sample http get request to codesee.io to see on it on the CodeSee Service Map
+http.get('http://codesee.io', (res) => {
+  res.on('data', (chunk) => {
+    console.log(res.statusCode);
+  });
+  res.on('end', () => {
+    console.log('done with http request');
+  });
+})
 
 module.exports.handler = async (event) => {
-
   return {
     statusCode: 200,
     body: JSON.stringify(
